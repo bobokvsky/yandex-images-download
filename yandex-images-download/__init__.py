7d17415bbac52f6acdf0f06f96e58d2740ee85bd
bobokvsky <alexander.kozlovsky.m@gmail.com>
2019-10-27 17:30:22 +0300

    PiPy

--- yandex-images-download/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-def run_main():
-    from yandex_images_download.yandex_images_download import main
-    main()
-
-if __name__ == '__main__':
-    run_main()
